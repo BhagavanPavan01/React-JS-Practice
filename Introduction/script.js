721094ac8415b9ad5dcfcb74db2ced4f3d6723d9
Bhagvan Pavan <bhagavanpavan999@gmail.com>
2025-04-29 19:58:24 +0530

    Learn React Concepts

---
+++ Introduction/script.js
@@ -0,0 +1,11 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const element = (
+  <div>
+    <h1 className="greeting">Hello, world!</h1>
+    <h2 className="greeting">Welcome to React!</h2>
+  </div>
+);
+
+root.render(element);
+ 
